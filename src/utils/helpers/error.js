d3f9ebe86f8b6f3f67931c8b19ed9fd25f34ee97
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-12-30 11:47:29 -0400

    add utils helpers

---
+++ src/utils/helpers/error.js
@@ -0,0 +1,12 @@
+class ErrorObject extends Error {
+  constructor(message, statusCode) {
+    super()
+    this.message = message
+    this.statusCode = statusCode
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+module.exports = {
+  ErrorObject
+}
