158420c18c86e3da6393fda23262bfc2cddb67b8
Alfredo Villegas <alfreddovillegas@gmail.com>
2023-01-19 23:09:06 -0400

    add validations of request schemas in auth routes

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const authRouter = express.Router()
 const authControllers = require('./authControllers')
+const validateReqSchema = require('../../middelwares/validateReqSchema')
+const { loginReqSchema, registerUserReqSchema } = require('./authReqSchemas')
 
-authRouter.post('/auth/register', authControllers.registerUser)
-authRouter.post('/auth/login', authControllers.login)
+authRouter.post(
+  '/auth/register',
+  validateReqSchema(registerUserReqSchema),
+  authControllers.registerUser
+)
+authRouter.post(
+  '/auth/login',
+  validateReqSchema(loginReqSchema),
+  authControllers.login
+)
 
 module.exports = authRouter
