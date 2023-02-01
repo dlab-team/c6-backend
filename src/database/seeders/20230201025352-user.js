2af15e49cb34fb8e8e069763b3570162e2491218
Alfredo Villegas <alfreddovillegas@gmail.com>
2023-01-31 23:09:45 -0400

    refactor: model, migrations and seeders

---
+++ src/database/seeders/20230201025352-user.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          name: 'dlab',
+          email: 'dlab@gmail.com',
+          password: '123456789',
+          isActive: true,
+          isAdmin: false,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
